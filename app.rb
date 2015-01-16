5311d49184e54593c6819d1495edc5d3595e0c49
Matthew Terrill <meterrill@gmail.com>
2015-01-16 20:33:04 +0000

    Add index file with a form to add contacts and list contact names.

--- app.rb
+++ app.rb
@@ -4,5 +4,14 @@ also_reload("lib/**/*.rb")
 require("./lib/contact")
 
 get("/") do
+  @contacts = Contact.all()
   erb(:index)
+end
+
+post("/contacts") do
+  name = params.fetch("name")
+  phone_number = params.fetch("phone_number")
+  contact = Contact.new(name, phone_number)
+  contact.save()
+  erb(redirect to("/"))
 end
