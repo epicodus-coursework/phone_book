5a53750e95e06b03d1fed7c01fe84febba642739
Matthew Terrill <meterrill@gmail.com>
2015-01-17 01:00:00 +0000

    Add forms/pages for home/work/cell numbers.

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/contact")
+require("./lib/phone")
 
 get("/") do
   @contacts = Contact.all()
@@ -10,14 +11,28 @@ end
 
 post("/contacts") do
   name = params.fetch("name")
-  phone_number = params.fetch("phone_number")
-  contact = Contact.new(name, phone_number)
-  contact.save()
+  Contact.new(name).save()
   @contacts = Contact.all()
-  erb(redirect to("/"))
+  erb(:index)
+end
+
+post("/phone_numbers") do
+  home_phone = params.fetch("home_phone")
+  work_phone = params.fetch("work_phone")
+  cell_phone = params.fetch("cell_phone")
+  @phone_number = Phone.new(home_phone, work_phone, cell_phone)
+  @phone_number.save()
+  @contact = Contact.find(params.fetch("contact_id").to_i())
+  @contact.add_phone_number(@phone_number)
+  erb(:contact)
+end
+
+get("/phone_numbers/:id") do
+  @phone_number = Phone.find(params.fetch("id").to_i())
+  erb(:phone_number)
 end
 
 get("/contacts/:id") do
-  @contact = Contact.find(params.fetch("id"))
+  @contact = Contact.find(params.fetch("id").to_i())
   erb(:contact)
 end
